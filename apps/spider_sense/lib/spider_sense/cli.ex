92cd7269226812cb34ba57e4bd33894ec75c3320
Andrey Akulov <andreyshark@gmail.com>
2020-09-13 00:55:28 +0300

    Make escript to return list of modules

--- apps/spider_sense/lib/spider_sense/cli.ex
+++ apps/spider_sense/lib/spider_sense/cli.ex
@@ -1,10 +1,28 @@
 defmodule SpiderSense.CLI do
   alias SpiderSense.DExplorer
+  alias SpiderSense.DGraph
 
-  def main(_args) do
-    DExplorer.subscribe()
-    DExplorer.start("mix.exs")
+  def main(args) do
+    path_to_mix_file = List.first(args) || "mix.exs"
 
-    # TODO: 
+    graph =
+      DExplorer.stream_events(path_to_mix_file)
+      |> Enum.reduce(%DGraph{}, fn event, graph ->
+        DGraph.process_event(graph, event)
+      end)
+
+    IO.puts("Modules: ")
+
+    DGraph.list_nodes(graph)
+    |> Enum.each(fn %{name: module} ->
+      IO.puts("- " <> to_string(module))
+    end)
+
+    IO.puts("Links: ")
+
+    DGraph.list_links(graph)
+    |> Enum.each(fn %{source: source, sink: sink} ->
+      IO.puts("- #{source} -> #{sink}")
+    end)
   end
 end
