188bb266fc24770148283cff9e5d83a8405daddd
Brandon Gottlob <info@bgottlob.com>
2020-09-11 22:18:24 -0400

    Replace cowboy with plug_cowboy

--- apps/spider_sense_web/mix.exs
+++ apps/spider_sense_web/mix.exs
@@ -43,7 +43,7 @@ defmodule SpiderSenseWeb.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"}
     ]
   end
 
