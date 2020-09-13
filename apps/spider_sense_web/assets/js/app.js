9d11e667cf95dae547496ff652617b43e15ce1df
Andrey Akulov <andreyshark@gmail.com>
2020-09-13 18:19:28 +0300

    Changed DGraph Live to subscript. Replace js interop with hooks

--- apps/spider_sense_web/assets/js/app.js
+++ apps/spider_sense_web/assets/js/app.js
@@ -25,8 +25,19 @@ import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 import { DGraph } from "./dgraph"
 
+let Hooks = {}
+Hooks.DGraph = {
+    chart() { return this.el.dataset.chart },
+    mounted() {
+        DGraph.update_chart("dgraph");
+    },
+    updated() {
+        DGraph.update_chart("dgraph");
+    }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
 // Connect if there are any LiveViews on the page
 liveSocket.connect()
--- apps/spider_sense_web/lib/spider_sense_web/live/dgraph_live.ex
+++ apps/spider_sense_web/lib/spider_sense_web/live/dgraph_live.ex
@@ -1,6 +1,7 @@
 defmodule SpiderSenseWeb.DGraphLive do
   use Phoenix.LiveView
 
+  alias SpiderSense.DExplorer
   alias SpiderSense.DGraph
 
   def render(assigns) do
@@ -8,78 +9,88 @@ defmodule SpiderSenseWeb.DGraphLive do
     <h3>Live value: <%= @counter %></h3>
     <form phx-submit="load_graph">
       <label for="path">Path to the <code>mix.exs</code> file</label>
-      <input type="text" name="path"></input>
+      <input type="text" name="mix_path" value="<%= @mix_path %>"></input>
       <button type="submit">Load Graph</button>
     </form>
     <%= if @loading do %>
       <div>Loading...</div>
     <% end %>
-    <div id="dgraph" data-chart="<%= Jason.encode!(@chart_data) %>">
+    <div id="dgraph" phx-hook="DGraph" data-chart="<%= Jason.encode!(@chart_data) %>">
       <svg width="600" height="600"></svg>
     </div>
-    <script>
-    // Since this script is loaded immediately, it must wait for app.js to be
-    // loaded, which inserts the DGraph constant into window
-    function wait() {
-      if (typeof DGraph !== "undefined") {
-        // Load the chart initially
-        DGraph.update_chart("dgraph");
-
-        // Update the DGraph chart whenever data changes
-        new MutationObserver(function(mutations, observer) {
-          DGraph.update_chart('dgraph');
-        }).observe(document.getElementById('dgraph'), {attributes: true});
-      }
-      else {
-        setTimeout(wait, 250);
-      }
-    }
-    wait();
-    </script>
     """
   end
 
   @impl Phoenix.LiveView
-  def handle_event("load_graph", %{"path" => path}, socket) do
-    # TODO: load graph here
+  def handle_event("load_graph", %{"mix_path" => mix_path}, socket) do
+    DExplorer.start(mix_path)
 
-    # kick off the graph loading
     {:noreply,
      socket
-     |> assign(:loading, true)}
+     |> assign(:mix_path, mix_path)
+     |> assign(:dgraph, %DGraph{})
+     |> update_chart_data_by_dgraph()
+    }
   end
 
   def mount(_params, _session, socket) do
-    # TODO: replace with subscribe
-    graph = SpiderSense.build_graph("../spider_sense/priv/example/mix.exs")
+    DExplorer.subscribe()
+    mix_path = "../spider_sense/priv/example/mix.exs"
+    DExplorer.start(mix_path)
+
+    {:ok,
+     socket
+     |> assign(:loading, false)
+     |> assign(:counter, 0)
+     |> assign(:mix_path, mix_path)
+     |> assign(:dgraph, %DGraph{})
+     |> update_chart_data_by_dgraph()
+    }
+  end
+
+  def handle_info({:explorer, :explore_project_start, _}, socket) do
+    {:noreply, assign(socket, :loading, true)}
+  end
+
+  def handle_info({:explorer, :explore_project_end, _}, socket) do
+    {:noreply, assign(socket, :loading, false)}
+  end
+
+  def handle_info({:explorer, _, _} = event, socket) do
+    socket =
+      socket
+      |> update_dgraph_by_event(event)
+      |> update_chart_data_by_dgraph()
+
+    {:noreply, socket}
+  end
+
+  defp update_dgraph_by_event(socket, event) do
+    dgraph = DGraph.process_event(socket.assigns.dgraph, event)
+    assign(socket, :dgraph, dgraph)
+  end
+
+  defp update_chart_data_by_dgraph(socket) do
+    dgraph = socket.assigns.dgraph
 
     nodes =
-      graph
-      |> DGraph.list_nodes()
+      DGraph.list_nodes(dgraph)
       |> Enum.reject(& is_nil(&1.name))
       |> Enum.map(fn %{name: module} ->
         %{id: module, group: 1}
       end)
 
     links =
-      DGraph.list_links(graph)
+      DGraph.list_links(dgraph)
       |> Enum.reject(& is_nil(&1.source))
       |> Enum.map(fn %{source: source, sink: sink} ->
         %{source: source, target: sink, value: 1}
       end)
 
-    {:ok,
-     socket
-     |> assign(:loading, false)
-     |> assign(:counter, 0)
-     |> assign(:chart_data, %{
-       nodes: nodes,
-       links: links
-     })}
-  end
-
-  def handle_info({:explorer, _, _}, socket) do
-    # TODO: use this
-    {:noreply, socket}
+    socket
+    |> assign(:chart_data, %{
+      nodes: nodes,
+      links: links
+    })
   end
 end
